9594ff9664ce706436181f6611e246d8df017529
nickshakh <shakhbazyann@gmail.com>
2023-10-25 21:40:28 +0400

    added some changes

--- homework2/my-app/src/App.js
+++ homework2/my-app/src/App.js
@@ -1,10 +1,13 @@
 // import logo from './logo.svg';
-// import './App.css';
-
+import './App.css';
+import Card from './Card';
+// import,Card
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
+        <h1>hi</h1>
+        <Card />
       </header>
     </div>
   );
---
+++ homework2/my-app/src/Card.js
@@ -0,0 +1,8 @@
+function Card () {
+    return(
+      <div className="CardDiv">
+        <p>Hello wordl!</p>
+      </div>  
+    )
+}
+export default Card
