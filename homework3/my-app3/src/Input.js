be88ddd4d607118b8916e88c7a5fde8900e218b6
nickshakh <shakhbazyann@gmail.com>
2023-10-29 15:00:15 +0400

    started with first part of the task

---
+++ homework3/my-app3/src/Input.js
@@ -0,0 +1,37 @@
+import React, { useState, useRef } from "react";
+
+function Input() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const emailRef = useRef(0);
+  const passwordRef = useRef(0);
+
+  const handleLogin = () => {
+    setLoggedIn(email === "digitalAcademy@gmail.com" && password === "iLoveReact123" ? true : false);
+
+  };
+
+  return (
+    <div>
+      {loggedIn ? (<div>
+          <h1>Welcome to the Main Site</h1>
+        </div>
+      ) : (
+        <div>
+          <h1>Login</h1>
+          <input type="text" placeholder="Email" ref={emailRef} onChange={(e) => setEmail(e.target.value)}/>
+          <br />
+          
+          <input type="password" placeholder="Password" ref={passwordRef} onChange={(e) => setPassword(e.target.value)}/>
+          <br />
+
+          <button disabled={!email || !password} onClick={handleLogin}>Login</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Input;
