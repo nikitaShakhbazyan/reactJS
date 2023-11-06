cfba820e481852f007ea2d679faf2c7f277477ed
nickshakh <shakhbazyann@gmail.com>
2023-11-07 01:24:33 +0400

    just started previous homework,made custom fetch hook and first page

---
+++ homework5/my-app5/src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='container'>
+       <h1> Georgian Universities</h1>
+       
+        </div>
+  )
+}
+
+export default Navbar
---
+++ homework5/my-app5/src/Pages/Home.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import useFetch from "../useFetch/useFetch"
+import styles from './home.module.css';
+import Navbar from '../Components/Navbar';
+import Details from './Details';
+import { Link } from "react-router-dom";
+
+
+const url = 'http://universities.hipolabs.com/search?country=Georgia'
+
+function Home () {
+    const { data, loading, error } = useFetch(url);
+
+      if (loading) {
+        return <div>Loading...</div>;
+      }
+    
+      if (error) {
+        return <div>Error: {error.message}</div>;
+      }
+
+
+
+      const mappedData = data.map((university) =>(
+        <div className={styles.container}>
+        <h2 key={university.id} style={{color:'red'}}>
+         {university.name}</h2>
+        <h4>Web Page : {university.web_pages[0]}</h4>
+        <h4>Domain : <a href={'http://' + university.domains}>{university.domains}</a></h4>
+
+        <button className={styles.buttonForMore}>
+            Click for more info
+            </button>
+        </div>
+
+      ))
+    
+      return (
+        <div className={styles.main}>
+            <Navbar/>
+        <div className={styles.inMain}>
+            {mappedData}
+        </div>
+
+        </div>
+      );
+    }
+
+export default Home
